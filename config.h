40e956cc8f0c34d9b2b1708b042b79a10c49ac5d
ilightwas <6960676+ilightwas@users.noreply.github.com>
2021-05-07 16:12:19 -0300

    Tag rules for google chrome

--- config.h
+++ config.h
@@ -68,6 +68,7 @@ static const Rule rules[] = {
 	/* class      instance    	title       tags mask     iscentered   isfloating   monitor */
 	{ "Gimp",     NULL,       	NULL,       0,            	0,           1,          	-1 },
 	{ "Firefox",  NULL,       	NULL,       1 << 8,       	0,           0,          	-1 },
+	{ "Google-chrome",NULL,     NULL,       1 << 7,       	0,           0,          	-1 },
 	{ "discord",  NULL,      	NULL,       1 << 2,       	0,           0,				-1 },
 	{ "Spotify",  NULL,  		NULL,      	1 << 3,       	0,           0,				-1 },
 	{ "Steam",    NULL,       	NULL,       1 << 4,       	0,           0,				-1 },
